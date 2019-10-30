45f9c21621698dfab96b578fbe2127641999b0a4
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-30 16:55:36 -0700

    Dashboard unit test complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
 // Test away
+afterEach(rtl.cleanup);
+
+test("Dashboard Renders both Controls and Display", () => {
+   const wrapper = rtl.render(<Dashboard />);
+   expect(wrapper.asFragment()).toMatchSnapshot();
+});
+
+// describe("Test Dashboard UI elements", () => {
+//    it("Matches the snapshot", () => {
+//       const wrapper = rtl.render(<Dashboard />);
+//       expect(wrapper.asFragment()).toMatchSnapshot();
+//    })
+// });
